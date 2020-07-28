fd9912d0fc391f658b1e95bf4047faac1799f0bd
oobbyy <153816742@qq.com>
2020-07-28 16:57:57 +0800

    数据库读取
    数据库读取

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -54,9 +54,30 @@ Page({
       success: res => {
         console.log('[云函数] [login] user openid: ', res.result.openid)
         app.globalData.openid = res.result.openid
-        if(res.result.openid == "opV-_4gbuoWst4l25jMHo9m8FXJ4"){
         wx.navigateTo({
           url: '../userConsole/userConsole',
+        })
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+        wx.navigateTo({
+          url: '../deployFunctions/deployFunctions',
+        })
+      }
+    })
+  },
+
+  createTeamwork: function() {
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res.result.openid)
+        app.globalData.openid = res.result.openid
+        if(res.result.openid == "opV-_4gbuoWst4l25jMHo9m8FXJ4"){
+        wx.navigateTo({
+          url: '../teamwork/createTeamwork',
         })}else{
           wx.navigateTo({
             url: '../userConsole/userConsole5',
---
+++ miniprogram/pages/teamwork/createTeamwork.js
@@ -0,0 +1,123 @@
+// miniprogram/pages/teamwork/createTeamwork.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    employees: [{
+        value: '1',
+        name: '美国'
+      },
+      {
+        value: '2',
+        name: '中国'
+      },
+      {
+        value: '3',
+        name: '巴西'
+      },
+      {
+        value: '4',
+        name: '日本'
+      },
+      {
+        value: '5',
+        name: '英国'
+      },
+      {
+        value: '6',
+        name: '法国'
+      }
+    ],
+    teachers: null
+
+  },
+
+  checkboxChange(e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
+
+    const employees = this.data.employees
+    const values = e.detail.value
+    for (let i = 0, lenI = employees.length; i < lenI; ++i) {
+      employees[i].checked = false
+
+      for (let j = 0, lenJ = values.length; j < lenJ; ++j) {
+        if (employees[i].value === values[j]) {
+          employees[i].checked = true
+          break
+        }
+      }
+    }
+
+    this.setData({
+      employees
+    })
+  },
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that=this;
+    const db = wx.cloud.database()
+    db.collection('todos').doc
+    ('2e31d63f5f1fd8b000038aa85d1e35cb').get({
+      success: function (res) {
+        console.log(res.data)
+        that.setData({teachers:res.data});
+      }
+    })
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
